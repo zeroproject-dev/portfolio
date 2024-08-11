93b5db82c8653fe8c6f3f97961deccca8489f96d
Andres Copeticona <zero@zeroproject.dev>
2024-08-11 14:02:18 -0400

    feat: update photos

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,3 +1,4 @@
+export * from "./utils";
 import english from "@i18n/locales/english.json";
 import spanish from "@i18n/locales/spanish.json";
 
@@ -14,12 +15,34 @@ export const getI18N = ({
   currentLocale = DEFAULT_LANG,
 }: {
   currentLocale: LocaleType | undefined;
-}) => {
+}): typeof english => {
   let langs = LANGS.filter((l) => l.locale === currentLocale);
 
   if (langs.length === 0) return english;
 
-  return { ...english, ...langs[0].json };
+  return deepMerge(english, langs[0].json);
 };
 
-export * from "./utils";
+function deepMerge(target: any, source: any): any {
+  const merged = { ...target };
+
+  for (const key in source) {
+    if (source.hasOwnProperty(key)) {
+      if (typeof source[key] === "object" && source[key] !== null) {
+        if (Array.isArray(source[key])) {
+          merged[key] = (merged[key] || []).map((item: any, index: any) =>
+            deepMerge(item, source[key][index]),
+          );
+        } else if (typeof merged[key] === "object" && merged[key] !== null) {
+          merged[key] = deepMerge(merged[key], source[key]);
+        } else {
+          merged[key] = deepMerge({}, source[key]) as any;
+        }
+      } else {
+        merged[key] = source[key];
+      }
+    }
+  }
+
+  return merged;
+}
